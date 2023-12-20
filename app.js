e356d4e36b0f50c1275316b07e37492321c90cd9
Diego de Radigues <20342@ecam.be>
2023-12-20 12:06:00 +0100

    commit détails

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Configurer EJS comme moteur de vue
 app.set('view engine', 'ejs');
 
-// Middleware pour parser le contenu des requêtes de type POST
 app.use(express.urlencoded({ extended: true }));
 
-// Utiliser les routes définies dans `vocabulaireRoutes.js`
 app.use('/', require('./routes/vocabulaireRoutes'));
 
-// Démarrer le serveur sur le port 3000
 app.listen(3000, () => {
     console.log('Serveur démarré sur http://localhost:3000');
 });
